0ee603c377d81b2a8c91cd139fb337fa9eb1f115
EDWIN AU <edwinau@rogers.com>
2017-04-02 11:28:58 -0400

    Class Time, name call works

--- people.rb
+++ people.rb
@@ -2,23 +2,24 @@ class Person
 
   attr_accessor :name, :group
 
-  @name = name
   @@group = []
 
-  def initialize
+  def initialize(name)
+      @name = name
+  end
+
+  def self.create(name)
       x = Person.new(name)
       @@group << x
-      return "Hi, my name is #{x}."
+      puts "Hi, my name is #{name}."
+      return x
   end
 end
 
-
 class Student < Person
-
   def learn
     return "I get it!"
   end
-
 end
 
 
@@ -27,3 +28,6 @@ class Instructor < Person
     return "Everything in Ruby is an Object."
   end
 end
+
+a=Instructor.create("Greg")
+b=Student.create("Edwin")
